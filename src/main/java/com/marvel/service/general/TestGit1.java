a60c4f50442151e19093d98cb9b57dfe835619a7
wulei <wenhuorongbing@126.com>
2018-10-25 00:05:49 +0800

    测试冲突6

--- src/main/java/com/marvel/service/general/TestGit1.java
+++ src/main/java/com/marvel/service/general/TestGit1.java
@@ -5,7 +5,8 @@ public class TestGit1 {
 	public static void main(String[] args) {
 		System.out.println("line 1");
 		System.out.println("line 2 changed");
-		System.out.println("line 4 another");
 		System.out.println("line 3 changed");
+		System.out.println("line 4 another");
+		
 	}
 }
