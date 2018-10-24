51feb488118b1ff32ffbccbc98f81ca815a7c0ec
wulei <wenhuorongbing@126.com>
2018-10-24 23:32:50 +0800

    冲突测试

--- src/main/java/com/marvel/service/general/TestGit1.java
+++ src/main/java/com/marvel/service/general/TestGit1.java
@@ -4,7 +4,7 @@ public class TestGit1 {
 	
 	public static void main(String[] args) {
 		System.out.println("line 1");
-		System.out.println("line 2");
+		System.out.println("line 2 changed");
 		System.out.println("line 3");
 	}
 }
