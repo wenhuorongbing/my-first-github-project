8e707f0f276852f452f7d384080bdb1ce1b12d76
wulei <wenhuorongbing@126.com>
2018-10-24 23:10:15 +0800

    提交测试

---
+++ src/main/java/com/marvel/service/general/TestGit1.java
@@ -0,0 +1,10 @@
+package com.marvel.service.general;
+
+public class TestGit1 {
+	
+	public static void main(String[] args) {
+		System.out.println("line 1");
+		System.out.println("line 2");
+		System.out.println("line 3");
+	}
+}
