7a5644036a8b255178464bacb4307fdb16ae67a0
wulei <wenhuorongbing@126.com>
2018-10-26 00:03:08 +0800

    提交00

--- src/main/java/com/marvel/service/general/TestGit2.java
+++ src/main/java/com/marvel/service/general/TestGit2.java
@@ -1,9 +1,9 @@
 package com.marvel.service.general;
 
-public class TestGit1 {
+public class TestGit2 {
 	
 	public static void main(String[] args) {
-		System.out.println("line 5A");
+		System.out.println("line 1 changed");
 		System.out.println("line 1 another");
 		System.out.println("line 2 changed");
 		System.out.println("line 3 changed");
