0a29bb35245f1c4f521ba76b1659d7f797694093
wulei <wenhuorongbing@126.com>
2018-10-26 00:54:38 +0800

    commit02

--- src/main/java/com/marvel/service/general/TestGit1.java
+++ src/main/java/com/marvel/service/general/TestGit1.java
@@ -3,7 +3,7 @@ package com.marvel.service.general;
 public class TestGit1 {
 	
 	public static void main(String[] args) {
-		System.out.println("line 5A");
+		System.out.println("line 5A changed");
 		System.out.println("line 1 another");
 		System.out.println("line 2 changed");
 		System.out.println("line 3 changed");
--- src/main/java/com/marvel/service/general/TestGit2.java
+++ src/main/java/com/marvel/service/general/TestGit2.java
@@ -3,10 +3,8 @@ package com.marvel.service.general;
 public class TestGit2 {
 	
 	public static void main(String[] args) {
-		System.out.println("line 1 changed");
 		System.out.println("line 1 another");
-		System.out.println("line 2 changed");
-		System.out.println("line 3 changed");
-		System.out.println("line 4 another");
+		System.out.println("line 2 another");
+		System.out.println("line 3 another");
 	}
 }
