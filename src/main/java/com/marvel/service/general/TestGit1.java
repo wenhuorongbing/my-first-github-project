8e85adb5503200225c3961fcd9e3dd4196fad651
wulei <wenhuorongbing@126.com>
2018-11-15 00:16:32 +0800

    冲突解决

--- src/main/java/com/marvel/service/general/TestGit1.java
+++ src/main/java/com/marvel/service/general/TestGit1.java
@@ -3,11 +3,9 @@ package com.marvel.service.general;
 public class TestGit1 {
 	
 	public static void main(String[] args) {
-		System.out.println("line 5B ");
-		System.out.println("line 5A changed");
 		System.out.println("line 1 another");
 		System.out.println("line 2 changed");
 		System.out.println("line 3 changed");
-		System.out.println("line 4 another");
+		System.out.println("line 5 another");
 	}
 }
