0d80e1ac199fdd128fdea2e39b2e7e627c86ab39
wulei <wenhuorongbing@126.com>
2018-11-18 22:50:18 +0800

    1

--- src/main/java/com/marvel/service/general/TestGit2.java
+++ src/main/java/com/marvel/service/general/TestGit2.java
@@ -4,9 +4,9 @@ public class TestGit2 {
 	
 	public static void main(String[] args) {
 		System.out.println("line 1 another");
-		System.out.println("line 22 another");
-		System.out.println("line 44 another");
-		System.out.println("line 2 another");
+		System.out.println("line a another");
+		System.out.println("line b another");
+		System.out.println("line c another");
 		System.out.println("line 44 another");
 	}
 }
