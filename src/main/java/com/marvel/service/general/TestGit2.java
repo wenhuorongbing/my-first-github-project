5e2d58694c1ac371c80fa5e0d0ad1925b5c65f55
wulei <wenhuorongbing@126.com>
2018-11-18 23:54:16 +0800

    22

--- src/main/java/com/marvel/service/general/TestGit2.java
+++ src/main/java/com/marvel/service/general/TestGit2.java
@@ -3,10 +3,10 @@ package com.marvel.service.general;
 public class TestGit2 {
 	
 	public static void main(String[] args) {
-		System.out.println("line 1 another");
+		System.out.println("good day");
 		System.out.println("line a another");
 		System.out.println("line hahaha another");
 		System.out.println("line c another");
-		System.out.println("line 44 another");
+		System.out.println("nice day");
 	}
 }
